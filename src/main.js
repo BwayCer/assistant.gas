d0590f7a128fab6822097975e7f065b52a76b6ad
BwayCer <bwaycer@gmail.com>
2020-10-11 16:40:32 +0800

    feat: 對 GasWebRecorder 新版的更改

--- src/main.js
+++ src/main.js
@@ -9,4 +9,5 @@ export * as timeStamp from './timeStamp.js';
 // gas
 export * as crypto from './crypto.js';
 export {default as Gasdb} from './Gasdb.js';
+export {default as GasWebRecorder} from './GasWebRecorder.js';
 
